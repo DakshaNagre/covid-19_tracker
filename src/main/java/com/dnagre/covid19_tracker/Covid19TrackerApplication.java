255515e60c667d051552aad5cf8f816872be5441
DakshaNagre <dnagre@iu.edu>
2020-09-07 16:19:37 -0400

    Initialized the project

---
+++ src/main/java/com/dnagre/covid19_tracker/Covid19TrackerApplication.java
@@ -0,0 +1,13 @@
+package com.dnagre.covid19_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Covid19TrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Covid19TrackerApplication.class, args);
+	}
+
+}
